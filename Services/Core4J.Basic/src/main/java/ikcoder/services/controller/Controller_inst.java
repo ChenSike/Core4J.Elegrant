a1ad58fd9cb48983199251728f8b2fcde9b76177
unknown <sike.chen@outlook.com>
2021-01-13 23:15:18 +0800

    add inst module

--- Services/Core4J.Basic/src/main/java/ikcoder/services/controller/Controller_inst.java
+++ Services/Core4J.Basic/src/main/java/ikcoder/services/controller/Controller_inst.java
@@ -1,10 +1,39 @@
 package ikcoder.services.controller;
 
+import ikcoder.entities.coredb_basic.DTI.DTI_inst;
+import ikcoder.entities.coredb_basic.DTO.DTO_common;
+import ikcoder.services.services.Services_common;
+import ikcoder.services.services.Services_inst;
+import ikcoder.services.services.Services_messages;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller_inst {
 
-    
+    Services_inst services_inst;
+    Services_messages services_messages;
+
+    @Autowired
+    public Controller_inst(Services_inst services_inst) {
+        this.services_inst = services_inst;
+    }
+
+    @PostMapping("/init")
+    @ResponseBody
+    public DTO_common NewInst(@RequestBody DTI_inst dti_inst)
+    {
+        if(services_inst.NewItem(dti_inst.getName(),dti_inst.getAddress(),dti_inst.getType(),dti_inst.getCity(),dti_inst.getCode()))
+        {
+            return Services_common.newCommonResItem(services_messages.GetMessage_code("8001"), "8001", false);
+        }
+        else
+        {
+            return Services_common.newCommonResItem(services_messages.GetMessage_code("4001"), "4001", true);
+        }
+    }
 
 }
--- Services/Core4J.Basic/src/main/java/ikcoder/services/services/Services_inst.java
+++ Services/Core4J.Basic/src/main/java/ikcoder/services/services/Services_inst.java
@@ -1,6 +1,7 @@
 package ikcoder.services.services;
 
 import ikcoder.entities.coredb_basic.DT.DT_inst;
+import ikcoder.entities.coredb_basic.DTI.DTI_inst;
 import ikcoder.services.mappers.Mapper_inst;
 import ikcoder.services.mappers.Mapper_messages;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,24 @@ public class Services_inst {
     public String GetFirstCode(Integer id) {
         DT_inst dt_inst = mapper_inst.select_inst_id(id);
         return dt_inst.getCode();
+    }
+
+    public boolean NewItem(String instName,String instAddress,String type,String city,String code)
+    {
+        try {
+            DTI_inst dti_inst = new DTI_inst();
+            dti_inst.setName(instName);
+            dti_inst.setAddress(instAddress);
+            dti_inst.setType(type);
+            dti_inst.setCity(city);
+            dti_inst.setCode(code);
+            mapper_inst.Insert_inst(dti_inst);
+            return true;
+        }
+        catch (Exception err)
+        {
+            return false;
+        }
     }
 
 }
